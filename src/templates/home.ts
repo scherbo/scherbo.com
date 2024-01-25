76c81148c611d7efa4ae8435b9aa1656937ac0a5
Sergey <kiniaevjason@gmail.com>
2024-01-25 18:13:32 +0100

    update header, footer and layout templates, home page template with mocked posts

--- src/templates/home.ts
+++ src/templates/home.ts
@@ -2,12 +2,33 @@ import { html } from "../utilities/html.ts";
 
 import { layoutTmpl } from "./layout.ts";
 
+const postList = [
+  { title: 'Some intriguing post name', date: '18-03-2024' },
+  { title: 'Another magnificent post name', date: '10-02-2024' },
+  { title: 'The last post in the list!!!', date: '28-01-2024' },
+]
+
 export function homeTmpl() {
   const content = html`
-    <main>
-      <div class="text-center">
-        <h2 class="text-xl text-main text-semibold">Hey! I'm Sergey Scherbo.</h2>
+    <main class="container">
+      <div style="margin-top: 150px; margin-bottom: 120px;">
+        <h2 class="text-xl text-main text-semibold mb-sm">Hey! I'm Sergey Scherbo.</h2>
         <h2 class="text-xl text-main text-semibold">And I do web stuff 🕸️</h2>
+      </div>
+
+      <!-- fake recent posts -->
+      <div>
+        <h3 class="text-lg text-main text-semibold mb-lg">Recent posts</h3>
+
+        <ul>
+          ${postList.map((post, i) => html`
+            <li class="flex justify-between">
+              <p class="text-sm text-main">${post.title}</p>
+              <p class="text-sm text-secondary">${post.date}</p>
+            </li>
+            ${i !== postList.length - 1 ? html`<hr class="my-md" />` : ''}
+          `).join('')}
+        </ul>
       </div>
     </main>
   `
