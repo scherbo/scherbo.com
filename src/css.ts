a9a35c57180c6869af98a8055b038ea86ca4e480
Sergey <kiniaevjason@gmail.com>
2024-02-10 19:21:03 +0100

    css dev script, link styles in head

---
+++ src/css.ts
@@ -0,0 +1,25 @@
+import { bundle } from "lightningcss";
+import { debounce } from "https://deno.land/std@0.207.0/async/debounce.ts";
+
+async function buildStyles() {
+  const { code } = bundle({
+    filename: "src/styles/main.css",
+    minify: true,
+  });
+
+  const styles = new TextDecoder().decode(code);
+
+  await Deno.writeTextFile(`${Deno.cwd()}/static/styles.css`, styles);
+}
+
+await buildStyles();
+
+console.log("Watching css...");
+
+const watcher = Deno.watchFs(`${Deno.cwd()}/src/styles`);
+
+const buildCall = debounce(buildStyles, 150);
+
+for await (const _event of watcher) {
+  buildCall();
+}
